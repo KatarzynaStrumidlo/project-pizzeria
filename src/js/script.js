cb12a33ca0249086d01719665b03f1e57a6be480
Katarzyna Strumidło <strumidlokd@gmail.com>
2021-02-07 14:51:30 +0100

    Remove products from cart

--- src/js/script.js
+++ src/js/script.js
@@ -347,6 +347,9 @@
       thisCart.dom.productList.addEventListener('updated', function(){
         thisCart.update();
       });
+      thisCart.dom.productList.addEventListener('remove', function(){
+        thisCart.remove(event.detail.cartProduct);
+      });
 
     }
     add(menuProduct){
@@ -387,8 +390,15 @@
       thisCart.dom.deliveryFee.innerHTML = deliveryFee;
 
     }
+    remove(cartProduct){
+      const thisCart = this;
+      const elementIndex = thisCart.products.indexOf(CartProduct);
+      thisCart.products.splice(elementIndex, 1);
+      cartProduct.dom.wrapper.remove();
+      thisCart.update();
+    }
   }
-  class CartProduct{ // eslint-disable-line no-unused-vars
+  class CartProduct{
 
     constructor(menuProduct, element) {
       const thisCartProduct = this;
@@ -402,6 +412,7 @@
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
       console.log('thisCartProduct:', thisCartProduct);
     }
 
@@ -424,6 +435,28 @@
         thisCartProduct.amount = thisCartProduct.amountWidget.value;
         thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+      });
+    }
+    remove(){
+      const thisCartProduct = this;
+
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
+    initActions(){
+      const thisCartProduct = this;
+      thisCartProduct.dom.edit.addEventListener('click', function(event){
+        event.preventDefault();
+      });
+      thisCartProduct.dom.remove.addEventListener('click', function(event){
+        event.preventDefault();
+        thisCartProduct.remove();
       });
     }
   }
